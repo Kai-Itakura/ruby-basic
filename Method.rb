e48a98dfe4b54db8c5142b117de501128f64d828
Kai-Itakura <itakai199969@gmail.com>
2023-04-22 11:52:35 +0900

    メソッドを学習

---
+++ Method.rb
@@ -0,0 +1,26 @@
+# def say_hello
+#   p "Hello World"
+# end
+
+# say_hello
+
+# def sayHello(greeting)
+#   p greeting
+# end
+
+# sayHello("Good morning")
+# sayHello("Good evening")
+
+# def add(num01, num02)
+#   return num01 + num02
+# end
+
+# add_result = add(6,8)
+# p add_result
+
+def average(num1, num2, num3)
+  return (num1 + num2 + num3) / 3
+end
+
+average_result = average(3,6,9)
+p average_result
