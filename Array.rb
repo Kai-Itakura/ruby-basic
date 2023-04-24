f74c26422872dc65e04696bfdb92cd227d52b64c
Kai-Itakura <itakai199969@gmail.com>
2023-04-24 14:23:28 +0900

    配列を学習

--- Array.rb
+++ Array.rb
@@ -16,9 +16,11 @@
 # p a[1]
 # p a[2]
 
+# arr = [["sato","suzuki"],["takahashi", "tanaka"]]
+# p arr[0][0]
+# p arr[0][1]
+# p arr[1][0]
+# p arr[1][1]
 
-arr = [["sato","suzuki"],["takahashi", "tanaka"]]
-p arr[0][0]
-p arr[0][1]
-p arr[1][0]
-p arr[1][1]
+names = ["Hanamichi", "Kaede", "Gori"]
+puts names
